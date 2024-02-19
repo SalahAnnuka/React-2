8216c893ba9644662e1a88058a3f813996c80f40
SalahAnnuka <158805101+SalahAnnuka@users.noreply.github.com>
2024-02-19 11:20:46 +0300

    create/update

---
+++ react2/src/Payments.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Payments.css';
+
+
+class Payments extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+
+            <div></div>
+        );
+    } 
+}
+export default Payments; 
