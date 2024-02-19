3bc97d18a698636781f86e9c3890f6bf5f02c36a
SalahAnnuka <158805101+SalahAnnuka@users.noreply.github.com>
2024-02-19 13:50:23 +0300

    update fonts

--- react2/src/App.js
+++ react2/src/App.js
@@ -21,6 +21,12 @@ function NavBar(){
 function App() {
   return (
     <Router>
+      <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+
+      </head>
       <div className="App">
         <NavBar/>
         <Routes>
