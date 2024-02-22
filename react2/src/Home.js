d54f81c974ee3b7512adc4d04ef2981184c6afbb
SalahAnnuka <158805101+SalahAnnuka@users.noreply.github.com>
2024-02-22 19:32:40 +0300

    update all

--- react2/src/Home.js
+++ react2/src/Home.js
@@ -5,16 +5,21 @@ import './Home.css';
 class Home extends React.Component{
     constructor(props){
         super(props);
+        this.formRef = React.createRef();
     }
+
+    submit(){
+        this.formRef.submit();
+    }
+
     render(){
         return(
             <div className="Home">
                 <h1 className="title">Login</h1>
-                <form className="login">
-                    <input type="text" placeholder="Username" className="field"/>
-                    <input type="password" placeholder="Password" className="field"/>
-                    <a className="submit" href="#">Login</a>
-
+                <form className="login" ref={this.formRef} >
+                    <input required type="text" placeholder="Username" className="field"/>
+                    <input required type="password" placeholder="Password" className="field"/>
+                    <a onClick={this.submit} type="submit" className="submit" href="#">Login</a>
                 </form>
             </div>
         );
